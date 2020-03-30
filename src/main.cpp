437846e772fa5951d121a8db303b8b975c11e27d
Bruno Santana <bruno.sant.a@gmail.com>
2020-03-30 12:50:12 -0300

    feat: verify if env is valid

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,42 @@
 #include "firmware.hpp"
 #include "command.hpp"
 
+std::string BOARDS[1] = {"rpi3"};
+std::string LANGS[1]  = {"python"};
+
 std::string env_board     = "";
 std::string env_lang      = "";
 
+void msg_env() {
+  std::cout << std::endl << "Environment variables" << std::endl << std::endl;
+  std::cout << std::endl << "\tNANOBRAIN_BOARD\t[rpi3]" << std::endl;
+  std::cout << "\tNANOBRAIN_LANG\t[python]" << std::endl << std::endl;
+}
+
 int get_env() {
   char * var;
+  int flag = 0;
   var = getenv("NANOBRAIN_BOARD");
   if(var != NULL){
     env_board = var;
     var = getenv("NANOBRAIN_LANG");
-    if(var != NULL){
-      env_lang = var;
-      return 1;
+    if(var != NULL) env_lang = var;
+    int size = sizeof BOARDS /sizeof BOARDS[0];
+    for (int i=0; i < size; i++) {
+      if(env_board.compare(BOARDS[i]) == 0) flag = 1;
     }
-  } else {
+    // Verify if is valid
+    if(flag) {
+      flag = 0;
+      size = sizeof LANGS /sizeof LANGS[0];
+      for (int i=0; i < size; i++) {
+        if(env_lang.compare(LANGS[i]) == 0) flag = 1;
+      }
+      if(flag) return 1;
+    }
+    std::cout << std::endl << "ERROR:" << std::endl;
+    std::cout << "BOARD=" << env_board << std::endl;
+    std::cout << "LANG=" << env_lang << std::endl;
     return 0;
   }
   return 0;
@@ -30,9 +52,8 @@ int main(int argc, char ** argv){
   std::cout << "NanoBrain - CLI" << std::endl;
 
   if (get_env() != 1) {
-    std::cout << std::endl << "Set the environment variables." << std::endl << std::endl;
-    std::cout << std::endl << "\tNANOBRAIN_BOARD\t[rpi3]" << std::endl;
-    std::cout << "\tNANOBRAIN_LANG\t[python]" << std::endl << std::endl;
+    std::cout << std::endl << "NOT SET:" << std::endl;
+    msg_env();
     return -1;
   }
   Project project = Project(env_board, env_lang);
@@ -71,6 +92,9 @@ int main(int argc, char ** argv){
         firmware.start();
       } else if(value == "stop") {
         firmware.stop();
+      } else if((value == "--help") || (value == "-h")){
+
+        return (1);
       }
     } else {
       return (0);
